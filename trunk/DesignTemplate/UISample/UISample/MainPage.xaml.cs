8951f80d28130a5da706dab017351203b02b10fc
kensuya89 <kensuya89@a676552f-7e26-0a9b-835f-54b056d88a0f>
2010-05-20 15:31:00 +0000

    hien thi vocabulary, ket noi voi csdl dung linq

--- trunk/DesignTemplate/UISample/UISample/MainPage.xaml.cs
+++ trunk/DesignTemplate/UISample/UISample/MainPage.xaml.cs
@@ -7,21 +7,26 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+
 
 namespace UISample
 {
 	public partial class MainPage : UserControl
 	{
-        bool flagUnit1 = true;
-        bool flagUnit2 = true;
-        bool flagUnit3 = true;
+        ServiceReference1.Service123Client client = new ServiceReference1.Service123Client();
 
-		public MainPage()
-		{
-			// Required to initialize variables
-			InitializeComponent();
+        public MainPage()
+        {
+            // Required to initialize variables
+            InitializeComponent();
             strbShowUnitGrid.Begin();
-            
+
             //===moi them vao===//
             rtbxReading.IsReadOnly = true;
             rtbxReading.Visibility = Visibility.Collapsed;
@@ -33,98 +38,99 @@ namespace UISample
             rpcReadingPractice.LoadContentFromXml("Data/Reading/Practice/reading_practice_3.xml");
             rpcReadingPractice.TittleAlignment(TextAlignment.Left);
             rpcReadingPractice.Visibility = Visibility.Collapsed;
-		}
-		
-		//unit 1
-		private void rctUnit1_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-            if (flagUnit1)
-                rctUnit1.Opacity = 0;
-		}
 
-		private void rctUnit1_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-            if (flagUnit1)
-                rctUnit1.Opacity = 0.5;
-		}
+        }
 		
-		private void rctUnit1_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-            //strbShowUnitGrid.AutoReverse = true;
-            rctUnit1.Opacity = 0.5;
-            rctUnit2.Opacity = 0;
-            rctUnit3.Opacity = 0;
-            flagUnit1 = false;
-            flagUnit2 = true;
-            flagUnit3 = true;
-		}
-		
-		//unit 2
-		private void rctUnit2_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-            if (flagUnit2)
-                rctUnit2.Opacity = 0;
-		}
+        ////unit 1
+        //private void rctUnit1_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        //{
+        //    // TODO: Add event handler implementation here.
+        //    if (flagUnit1)
+        //        rctUnit1.Opacity = 0;
+        //}
 
-		private void rctUnit2_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-            if (flagUnit2)
-                rctUnit2.Opacity = 0.5;
-		}
+        //private void rctUnit1_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        //{
+        //    // TODO: Add event handler implementation here.
+        //    if (flagUnit1)
+        //        rctUnit1.Opacity = 0.5;
+        //}
 		
-		private void rctUnit2_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-            rctUnit2.Opacity = 0.5;
-            rctUnit1.Opacity = 0;
-            rctUnit3.Opacity = 0;
-            flagUnit2 = false;
-            flagUnit1 = true;
-            flagUnit3 = true;
-		}
+        //private void rctUnit1_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        //{
+        //    // TODO: Add event handler implementation here.
+        //    //strbShowUnitGrid.AutoReverse = true;
+        //    rctUnit1.Opacity = 0.5;
+        //    rctUnit2.Opacity = 0;
+        //    rctUnit3.Opacity = 0;
+        //    flagUnit1 = false;
+        //    flagUnit2 = true;
+        //    flagUnit3 = true;
+        //}
 		
-		//unit 3
-		private void rctUnit3_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-            if (flagUnit3)
-                rctUnit3.Opacity = 0;
-		}
-		
-		private void rctUnit3_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-            if (flagUnit3)
-                rctUnit3.Opacity = 0.5;
-		}
+        ////unit 2
+        //private void rctUnit2_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        //{
+        //    // TODO: Add event handler implementation here.
+        //    if (flagUnit2)
+        //        rctUnit2.Opacity = 0;
+        //}
 
-		private void rctUnit3_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-            rctUnit3.Opacity = 0.5;
-            rctUnit1.Opacity = 0;
-            rctUnit2.Opacity = 0;
-            flagUnit3 = false;
-            flagUnit1 = true;
-            flagUnit2 = true;
-		}
+        //private void rctUnit2_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        //{
+        //    // TODO: Add event handler implementation here.
+        //    if (flagUnit2)
+        //        rctUnit2.Opacity = 0.5;
+        //}
+		
+        //private void rctUnit2_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        //{
+        //    // TODO: Add event handler implementation here.
+        //    rctUnit2.Opacity = 0.5;
+        //    rctUnit1.Opacity = 0;
+        //    rctUnit3.Opacity = 0;
+        //    flagUnit2 = false;
+        //    flagUnit1 = true;
+        //    flagUnit3 = true;
+        //}
+		
+        ////unit 3
+        //private void rctUnit3_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        //{
+        //    // TODO: Add event handler implementation here.
+        //    if (flagUnit3)
+        //        rctUnit3.Opacity = 0;
+        //}
+		
+        //private void rctUnit3_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        //{
+        //    // TODO: Add event handler implementation here.
+        //    if (flagUnit3)
+        //        rctUnit3.Opacity = 0.5;
+        //}
+
+        //private void rctUnit3_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        //{
+        //    // TODO: Add event handler implementation here.
+        //    rctUnit3.Opacity = 0.5;
+        //    rctUnit1.Opacity = 0;
+        //    rctUnit2.Opacity = 0;
+        //    flagUnit3 = false;
+        //    flagUnit1 = true;
+        //    flagUnit2 = true;
+        //}
 		private void btnTriangle_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
 			// TODO: Add event handler implementation here.
-            cnvReading.Opacity = 0;
+            //cnvReading.Opacity = 0;
             strbShowReadingContentReverse.Begin();
+            strbShowNewVocabolaryReverse.Begin();
 		}
 
         private void bntVietnam_Click(object sender, RoutedEventArgs e)
         {
             rtbxReading.RichText = "Đây là RichTextBox mình vừa add dll vào. H mọi người code phần reading trên canvas này nhe. Có thiếu control ji cứ thêm vào. Nhóm Design sẽ chỉnh sửa lại sao. Nếu ko có ji thì đừng nên &#xd;&#xa;dịch chuyển vị trí các control nhe(amination chạy loạn á mà).&#xd;&#xa;&#xd;&#xa;À 1 vấn đề là khi hiển thị thì phan nội dung bên phải cửa sổ chương trình nó vẫn hiện lên=&gt; nên khi chạy nhin thay hết canvas Reading (xấu lắm). Tụi mình chưa sửa dc nên dùng 1 rectangle màu đen che đỡ. Bạn nao biết chỉ dùm lun.&#xd;&#xa;Thank all.";
             rtbxReading.Visibility = Visibility.Visible;
-
             rpcReadingPractice.Visibility = Visibility.Collapsed;
         }
 
@@ -141,6 +147,28 @@ namespace UISample
             rpcReadingPractice.Visibility = Visibility.Visible;
 
             rtbxReading.Visibility = Visibility.Collapsed;
+        }
+
+        private void mbt_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Storyboard sb = (this.Resources["strbShowReadingContent"] as Storyboard);
+            sb.Begin();
+        }
+
+        public void client_GetVocabularyCompleted(object sender,ServiceReference1.GetVocabularyCompletedEventArgs e)
+        {
+            dg_NewVocabulary.ItemsSource = e.Result.ToList();
+        }
+
+        private void bnt_New_Vocabulary_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            int a;
+            a = 1;
+            strbShowNewVocabolary.Begin();
+            client.GetVocabularyCompleted += new EventHandler<ServiceReference1.GetVocabularyCompletedEventArgs>(client_GetVocabularyCompleted);
+            //can bit dang o bai nao thi load vocabolary bai do len, truyen tham so vao
+            //vi du o day la a
+            client.GetVocabularyAsync(a);
         }
 	}
 }
---
+++ trunk/DesignTemplate/UISample/UISampleSite/App_Code/Service123.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Linq;
+
+[ServiceContract(Namespace = "")]
+[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+public class Service123
+{
+    [OperationContract]
+    public List<VOCABULARY> GetVocabulary(int a)
+    {
+        AV10DataContext db = new AV10DataContext();
+        var voca = from item in db.VOCABULARies select item;
+        
+        //var queue = from q in dc.SomeTable
+        //where q.SomeDate <= DateTime.Now && q.Locked != true
+        //orderby (q.Priority, q.TimeCreated)
+        //select q;
+
+        //var queue2 = dc.SomeTable
+        //.Where( q => q.SomeDate <= DateTime.Now && q.Locked != true )
+        //.OrderBy(q => q.Priority)
+        //.ThenBy(q => q.TimeCreated);
+
+
+        return voca.ToList() ;
+        //cai chuan truy van nay goi la lambda expression
+        //return db.VOCABULARies.Where(VOCABULARY => VOCABULARY.ID_Voc == a).ToList();
+    }
+
+    // Add more operations here and mark them with [OperationContract]
+}
